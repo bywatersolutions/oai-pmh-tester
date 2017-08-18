aea4072ee4d6acd49394344081a3445fa86da1c5
Kyle M Hall <kyle@bywatersolutions.com>
2017-08-18 03:01:50 +0000

    Switch to HTTP::OAI

--- oai-pmh-tester.pl
+++ oai-pmh-tester.pl
@@ -1,21 +1,21 @@
 #!/usr/bin/perl
 
 use Modern::Perl;
-use Net::OAI::Harvester;
 use Getopt::Long;
+use HTTP::OAI;
 
 my $verbose = 0;
 my $base_url;
 my $identifier;
-my $archive_id = 'KOHA-OAI-TEST';
-my $metadataPrefix = 'marcxml';
+my $archive_id      = 'KOHA-OAI-TEST';
+my $metadata_prefix = 'marcxml';
 
 GetOptions(
     "u|url=s"             => \$base_url,
     "a|archive-id=s"      => \$archive_id,
     "i|identifier=s"      => \$identifier,
-    "m|metadata-prefix=s" => \$metadataPrefix,
-    "v|verbose"           => \$verbose
+    "m|metadata-prefix=s" => \$metadata_prefix,
+    "v|verbose+"          => \$verbose
 ) or die("Error in command line arguments\n");
 
 unless ( $base_url && $identifier ) {
@@ -23,46 +23,20 @@ unless ( $base_url && $identifier ) {
     exit 1;
 }
 
-my $harvester = Net::OAI::Harvester->new( baseURL => $base_url );
+my $h = new HTTP::OAI::Harvester( baseURL => $base_url );
 
-my $identity = $harvester->identify();
-say "SERVER: ";
-say " name: ", $identity->repositoryName();
-
-my $result = $harvester->getRecord(
+my $gr_response = $h->GetRecord(
     identifier     => "$archive_id:$identifier",
-    metadataPrefix => $metadataPrefix,
+    metadataPrefix => $metadata_prefix,
 );
+my $record = $gr_response->next;
+die $record->message if $record->is_error;
 
-## get the result as Net::OAI::Record object
-my $record = $result->record();    # undef if error
-say "RECORD: $record";
-say $record->recorddata();
+say "RECORD:";
+say "  Identifier: ", $record->identifier();
+say "   DateStamp: ", $record->datestamp();
+say "      Status: ", $record->status() || q{};
+say "     Deleted: ", $record->is_deleted();
 
-## directly get the Net::OAI::Record::Header object
-my $header = $result->header();    # undef if error
-say "HEADER:";
-say " status: ",     $header->status();
-say " identifier: ", $header->identifier();
-say " datestamp: ",  $header->datestamp();
-
-## get the metadata object
-my $metadata =
-  $result->metadata();    # undef if error or harvested with recordHandler
-say "METADATA:";
-say "  identifier: ", $header->identifier();
-say "       title: ", $metadata->title();
-say "     creator: ", $metadata->creator();
-say "     subject: ", $metadata->subject();
-say " description: ", $metadata->description();
-say "   publisher: ", $metadata->publisher();
-say " contributor: ", $metadata->contributor();
-say "        date: ", $metadata->date();
-say "        type: ", $metadata->type();
-say "      format: ", $metadata->format();
-say "      source: ", $metadata->source();
-say "    language: ", $metadata->language();
-say "    relation: ", $metadata->relation();
-say "    coverage: ", $metadata->coverage();
-say "      rights: ", $metadata->rights();
-
+my $dom = $record->metadata->dom;
+say "\nRECORD DOM:\n", $dom if $verbose;
